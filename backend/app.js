018938beb855e0b61e38990fad6442c6250eb20c
Doming0 <domingo2702@outlook.com>
2025-03-24 20:53:13 -0400

    Add initial backend configuration and dependencies

--- backend/app.js
+++ backend/app.js
@@ -35,3 +35,11 @@ mongoose.connect(process.env.MONGO_URI)
     });
   })
   .catch((err) => console.error("❌ Error al conectar a MongoDB:", err));
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
